c679607e27bcc07aca863881f6aa37cecbf02d6f
Leandro <l.trinidad@share.epsb.ca>
2018-06-07 23:15:59 -0600

    update git

---
+++ Run2/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed = 3.0f;
+	private void Start()
+	{
+        rb = GetComponent<Rigidbody>();
+
+	}
+	void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
+
+        rb.AddForce(movement * speed);
+    }
+}
