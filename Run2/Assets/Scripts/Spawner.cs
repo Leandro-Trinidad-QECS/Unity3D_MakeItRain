012b20dcfa5318999bf5202f12ca14842c66bbfd
Leandro <l.trinidad@share.epsb.ca>
2018-06-09 17:47:47 -0600

    update

---
+++ Run2/Assets/Scripts/Spawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject[] coins;
+    public Vector3 spawnValues;
+    public float spawnWait;
+    public float spawnMostWait;
+    public float spawnLeastWait;
+    public int startWait;
+    public bool stop;
+    public int randSpawn;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(Spawn());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        spawnWait = Random.Range(spawnLeastWait, spawnMostWait);
+	}
+    IEnumerator Spawn() {
+        yield return new WaitForSeconds(startWait);
+        while(!stop) {
+            //randSpawn = Random.Range(0, 2);
+            Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x),1,0);
+
+
+            Instantiate(coins[randSpawn], spawnPosition + transform.TransformPoint(0,0,0), gameObject.transform.rotation);
+
+            yield return new WaitForSeconds(spawnWait);
+        }
+    }
+}
---
+++ Run2/Assets/coinController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coinController : MonoBehaviour {
+    public float sizey;
+    new private Collider collider;
+	// Use this for initialization
+	void Start () {
+        collider = GetComponent<Collider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0, 5, 0, Space.World);
+
+        if(isGround()) {
+            StartCoroutine(Destroyobj());
+        }
+
+	}
+
+    IEnumerator Destroyobj() {
+        yield return new WaitForSeconds(5);
+        Destroy(gameObject);
+    }
+
+    bool isGround()
+    {
+        // checs if the user is touching the ground using raycast
+        Ray ray = new Ray(transform.position, -transform.up);
+        RaycastHit hit;
+        sizey = (collider.bounds.size.y / 2) + 0.1f;
+        if (Physics.Raycast(ray, out hit, sizey))
+        {
+            return true;
+        }
+        return false;
+    }
+}
