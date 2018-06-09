e462b93ea65d28115d8f348060ce3d3cfd7292b4
Leandro <l.trinidad@share.epsb.ca>
2018-06-08 18:55:31 -0600

    update

--- Run2/Assets/Scripts/PlayerMovement.cs
+++ Run2/Assets/Scripts/PlayerMovement.cs
@@ -3,17 +3,29 @@
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody rb;
-    public float speed = 3.0f;
+    private Collider collider;
+    public float sizey;
 	private void Start()
 	{
         rb = GetComponent<Rigidbody>();
+        collider = GetComponent<Collider>();
 
 	}
-	void FixedUpdate()
+	void Update()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
-
-        rb.AddForce(movement * speed);
+        if(isGround() && Input.GetButton("Jump")) {
+            rb.AddForce(0.0f,1.0f*2,0,ForceMode.Impulse);
+        }
+        //raycast to gorund to detect if on ground
+       
+    }
+    bool isGround() {
+        Ray ray = new Ray(transform.position, -transform.up);
+        RaycastHit hit;
+        sizey = (collider.bounds.size.y / 2) + 0.1f;
+        if(Physics.Raycast(ray, out hit, sizey)) {
+            return true;
+        }
+        return false;
     }
 }
