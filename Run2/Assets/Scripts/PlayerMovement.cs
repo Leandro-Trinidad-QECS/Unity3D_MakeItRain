c4e1686e0e0cddcf469e50bca91ce64d250f4777
Leandro <l.trinidad@share.epsb.ca>
2018-06-08 21:43:37 -0600

    update

--- Run2/Assets/Scripts/PlayerMovement.cs
+++ Run2/Assets/Scripts/PlayerMovement.cs
@@ -5,21 +5,46 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody rb;
     private Collider collider;
     public float sizey;
-	private void Start()
-	{
-        rb = GetComponent<Rigidbody>();
-        collider = GetComponent<Collider>();
 
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+    [Range(1,10)]
+    public float jumpVelocity;
+    public float speed;
+
+	private void Awake()
+	{
+        collider = GetComponent<Collider>();
+        rb = GetComponent<Rigidbody>();
 	}
+
 	void Update()
     {
-        if(isGround() && Input.GetButton("Jump")) {
-            rb.AddForce(0.0f,1.0f*2,0,ForceMode.Impulse);
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            rb.velocity = Vector3.left * speed;
+
         }
-        //raycast to gorund to detect if on ground
-       
+        
+        // jump button
+        if(isGround() && Input.GetButtonDown("Jump")) {
+            rb.velocity = Vector3.up * jumpVelocity;
+        } else {
+            if(Input.GetKeyDown(KeyCode.A)) {
+            rb.velocity = Vector3.left * speed;
+            
+        }
+        }
+        if(rb.velocity.y < 0) {
+            rb.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+
+        } else if(rb.velocity.y > 0 && !Input.GetButton("Jump")) {
+            rb.velocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+
     }
     bool isGround() {
+        // checs if the user is touching the ground using raycast
         Ray ray = new Ray(transform.position, -transform.up);
         RaycastHit hit;
         sizey = (collider.bounds.size.y / 2) + 0.1f;
